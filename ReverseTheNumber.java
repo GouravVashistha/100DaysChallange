d43e9a2f4157ec0fdf7e6ec78db236ad7acae971
Gourav Vashistha <59836501+GouravVashistha@users.noreply.github.com>
2023-04-07 14:44:24 +0530

    Create ReverseTheNumber.java

---
+++ ReverseTheNumber.java
@@ -0,0 +1,19 @@
+package Basic;
+
+public class ReverseTheNumber {
+
+	public static void main(String[] args) {
+		int a = 1234;
+		int rev = 0;
+   // using algo
+		while (a != 0) {
+			rev = rev * 10 + a % 10; //
+			a = a /10 ;
+		}
+		System.out.println(rev);
+  // using stringBuffer method
+		StringBuffer sv = new StringBuffer(String.valueOf(a));
+		System.out.println(sv.reverse());
+	}
+
+}
