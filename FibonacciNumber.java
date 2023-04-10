690d7fcd5e72f83e2db6349f75f469fc627d0d8e
Gourav Vashistha <59836501+GouravVashistha@users.noreply.github.com>
2023-04-10 17:07:28 +0530

    Create FibonacciNumber.java

---
+++ FibonacciNumber.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+public class FibonacciNumber {
+
+	public static void main(String[] args) {
+		int a=0;
+		int b=1;
+		int count = 10;
+		System.out.print(a+" "+b);
+		for(int i=2;i<count;i++) {
+			int c = a+b;
+			System.out.print(" "+ c);
+			a=b;
+			b=c;
+		}
+
+	}
+
+}
