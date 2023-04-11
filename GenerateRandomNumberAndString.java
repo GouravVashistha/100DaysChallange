6f19ff91843e8fcb39a2b6494a3d7abc635d5499
Gourav Vashistha <59836501+GouravVashistha@users.noreply.github.com>
2023-04-11 12:24:18 +0530

    Create GenerateRandomNumberAndString.java

---
+++ GenerateRandomNumberAndString.java
@@ -0,0 +1,20 @@
+package Basic;
+
+import java.util.Random;
+
+public class GenerateRandomNumberAndString {
+
+	public static void main(String[] args) {
+		// one method
+		Random rand =new Random();
+		int newnum = rand.nextInt(10);
+		System.out.println(newnum);
+		
+		// second method
+		
+		System.out.println(Math.random());
+		
+		// 3rd method
+	}
+
+}
