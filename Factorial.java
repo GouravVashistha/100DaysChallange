f32829da5992930c9cc69a06f96d0c37a44facf5
Gourav Vashistha <59836501+GouravVashistha@users.noreply.github.com>
2023-04-11 12:25:58 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,20 @@
+package Recursion;
+
+import java.util.*;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		int i, fact = 1;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Number :");
+		int num = sc.nextInt();
+		sc.close();
+		for (i = 1; i <= num; i++) {
+			fact = fact * i;
+		}
+		System.out.println(fact);
+		;
+	}
+
+}
