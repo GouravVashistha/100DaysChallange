2642d22ec7015fdb6a8b600eeaac9bfe1b739813
Gourav Vashistha <59836501+GouravVashistha@users.noreply.github.com>
2023-04-14 16:52:14 +0530

    Update DuplicatElementInArrays.java

--- DuplicatElementInArrays.java
+++ DuplicatElementInArrays.java
@@ -1,22 +1,41 @@
 package Basic;
 
+import java.util.HashSet;
+
 public class DuplicatElementInArrays {
 
 	public static void main(String[] args) {
 
 		String a[] = { "java", "python", "C++", "c", "python", "React", "Java" };
-		 boolean flag = false;
-		for (int i = 0; i < a.length; i++) {
-			for (int j = i + 1; j < a.length; j++) {
-				if (a[i] == a[j]) {
-					System.out.println("Found the duplicate Element :" + a[i]);
-					flag=true;
-				}
+		
+		// one method
+//		 boolean flag = false;
+//		for (int i = 0; i < a.length; i++) {
+//			for (int j = i + 1; j < a.length; j++) {
+//				if (a[i] == a[j]) {
+//					System.out.println("Found the duplicate Element :" + a[i]);
+//					flag=true;
+//				}
+//			}
+//		}
+//		if (flag == false) {
+//			System.out.println("Element not found");
+//		}
+		
+		
+		// second method
+		boolean flag = false;
+		HashSet<String> lang = new HashSet();
+		for(String l:a) {
+			if(lang.add(l)==false){
+				System.out.println("Duplicate Element found :"+l);
+				flag = true;
 			}
 		}
-		if (flag == false) {
-			System.out.println("Element not found");
-		}
+		 if(flag==false ){
+			 System.out.println("Element not found");
+		 }
+		
 
 	}
 
